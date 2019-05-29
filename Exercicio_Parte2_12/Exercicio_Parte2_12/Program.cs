b1a48c134a9f678476990b05ab6e1899fd99f9f7
Alberto <albertomaronezio@hotmai.com>
2019-05-29 16:15:14 -0300

    Exercicio_Parte2_12

---
+++ Exercicio_Parte2_12/Exercicio_Parte2_12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Parte2_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero = 0;
+            int segundoNumero = 0;
+            int terceiroNumero = 0;
+
+            Console.Write("Digite o Primeiro Número: ");
+            primeiroNumero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Segundo Número: ");
+            segundoNumero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Terceiro Número: ");
+            terceiroNumero = int.Parse(Console.ReadLine());
+
+            if (primeiroNumero > segundoNumero && primeiroNumero > terceiroNumero)
+            {
+                Console.WriteLine("O maior Número é: {0}", primeiroNumero);
+            }
+
+            else if (segundoNumero > primeiroNumero && segundoNumero > terceiroNumero)
+            {
+                Console.WriteLine("O maior Número é: {0}", segundoNumero);
+            }
+
+            else
+            {
+                Console.WriteLine("O maior Número é: {0}", terceiroNumero);
+            }
+        }
+    }
+}
